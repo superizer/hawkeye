ff901399e437bd9a4acecae97eda2a67e36466c9
Attasuntorn Traisuwan <superizer.tone@gmail.com>
2012-12-06 16:57:01 +0700

    account_form

--- hawkeye/forms/account_form.py
+++ hawkeye/forms/account_form.py
@@ -6,12 +6,10 @@ from . import AbstactForm
 class AccountForm(AbstactForm):
     email       = fields.TextField('Email', validators=[validators.required(), validators.Email()])
     password    = fields.PasswordField('Password', validators=[validators.required()])
-    #name
-    #surname
     
 class RegisterForm(AbstactForm):
     name     = fields.TextField('Name', validators=[validators.required()])
-    surname     = fields.TextField('Surname', validators=[validators.required()])
+    surname  = fields.TextField('Surname', validators=[validators.required()])
     email    = fields.TextField('Email', validators=[validators.required(), validators.Email()])
     password = fields.PasswordField('New Password', [validators.Required(), validators.EqualTo('confirm', message='Passwords must match')])
     confirm  = fields.PasswordField('Repeat Password')
