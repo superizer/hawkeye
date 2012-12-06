ed45daf8905df29f7fd0f365cad515ca6999fb3c
Attasuntorn Traisuwan <superizer.tone@gmail.com>
2012-12-06 14:29:10 +0700

    fix __init__ in nokkhum

--- hawkeye/forms/account_form.py
+++ hawkeye/forms/account_form.py
@@ -1,8 +1,14 @@
 from wtforms import fields
+from wtforms import fields
 from wtforms import validators
 
 from . import AbstactForm
 
 class AccountForm(AbstactForm):
     email       = fields.TextField('Email', validators=[validators.required(), validators.Email()])
-    password    = fields.PasswordField('Password', validators=[validators.required()])
+    password    = fields.TextField('Password', validators=[validators.required()])
+    #name
+    #surname
+    password = fields.PasswordField('New Password', [Required(), EqualTo('confirm', mesage='Passwords must match')])
+    confirm  = fields.PasswordField('Repeat Password')
+    
