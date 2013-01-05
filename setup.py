f1ad32b6f012b1204d9b0af1009078d162dfea66
Thanathip Limna <boatkrap@gmail.com>
2013-01-05 22:39:02 +0700

    create setup file

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'mako',
+    'requests',
+    'wtforms'
+    ]
+
+SCRIPT_NAME = 'hawkeye'
+setup(name='hawkeye',
+      version='0.0',
+      description='hawkeye nokkhum client',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: hawkeye",
+        ],
+      author='',
+      author_email='',
+      scripts = ['bin/%s' % SCRIPT_NAME],
+      license = 'xxx License',
+      packages = find_packages(),
+      url='https://github.com/superizer/hawkeye',
+      keywords='VSaaS, client',
+#      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+#      tests_require=requires,
+#      test_suite="nokkhum-controller",
+      )
+
